05a98f7cd2194a0d009ddf524e7a73f13306b9d5
Pedro Parronchi <pedrohenrique@univem.edu.br>
2022-03-20 09:50:45 -0300

    feat: adicionado DTOS e services na camada de apllcation

--- CleanArchMvc.WebUi/Startup.cs
+++ CleanArchMvc.WebUi/Startup.cs
@@ -14,6 +14,7 @@ namespace CleanArchMvc.WebUi
         {
             services.AddInfrastructure(Configuration);
             services.AddControllersWithViews();
+            //services.AddAutoMapper(typeof());
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
